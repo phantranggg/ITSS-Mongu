0e2bd49e62983d18b54c66904cddebee49b8b53f
Doan The Vu <thevudoan@gmail.com>
2019-11-06 15:50:19 +0000

    Update code v1

--- mizugi/config/routes.rb
+++ mizugi/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  get 'products/index'
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  root to: 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- mizugi/db/schema.rb
+++ mizugi/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_151914) do
+ActiveRecord::Schema.define(version: 2019_10_23_170411) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,6 +30,28 @@ ActiveRecord::Schema.define(version: 2019_10_23_151914) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "product_id"
+    t.integer "quantity"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_details_on_order_id"
+    t.index ["product_id"], name: "index_order_details_on_product_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "date"
+    t.string "ship_name"
+    t.text "ship_address"
+    t.boolean "shipped"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
