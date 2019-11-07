627c4c9bc96aac66630f1fc89ab77673042190f4
Tran Son Tung <tungts1101@gmail.com>
2019-11-07 04:37:27 +0000

    fix multiple upload with rails admin

--- mizugi/db/schema.rb
+++ mizugi/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_170411) do
+ActiveRecord::Schema.define(version: 2019_11_06_150805) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2019_10_23_170411) do
     t.boolean "live"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "images"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
--- mizugi/db/seeds.rb
+++ mizugi/db/seeds.rb
@@ -15,10 +15,11 @@ num_products = Category.all.length * 50
 
 for _ in 1..num_products
     Product.create(
-        name: Faker::Name.name,
-        price: rand( 1000 ),
+        name: Faker::Commerce.product_name,
+        price: Faker::Commerce.price(range: 100.0..1000.0),
         description: Faker::Lorem.sentence,
         category_id: (1..Category.all.length).to_a.sample,
-        live: (rand < 0.5) ? true : false
+        live: (rand < 0.5) ? true : false,
+        images: Array.new(rand(1..5)) { Rails.root.join("public/images/products/#{1+rand(5)}.jpg").open }
     )
 end
