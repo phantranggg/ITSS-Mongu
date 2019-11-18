543b36644e0dc84fee3d543b47e53628334f6d98
EC2 Default User <ec2-user@ip-172-31-86-137.ec2.internal>
2019-11-18 16:04:50 +0000

    Add checkout & confirm slim

--- mizugi/config/routes.rb
+++ mizugi/config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   root to: 'products#index'
   get '/cart', to: 'carts#index'
   get '/category', to: 'categories#index'
+  get '/checkout', to: 'payments#checkout'
   
   devise_for :admins, controllers: {
     sessions: 'admins/sessions'
