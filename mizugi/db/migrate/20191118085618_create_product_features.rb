e82e48f3ff027a682b3f825a983e2d20e0ce89e4
Tran Son Tung <tungts1101@gmail.com>
2019-11-18 09:03:18 +0000

    #40 redesign database

---
+++ mizugi/db/migrate/20191118085618_create_product_features.rb
@@ -0,0 +1,14 @@
+class CreateProductFeatures < ActiveRecord::Migration[5.2]
+  def change
+    create_table :product_features do |t|
+      t.references :product,   foreign_key: true
+      t.string :size
+      t.string :color
+      t.integer :gender
+      t.float :price
+      t.integer :quantity
+      
+      t.timestamps
+    end
+  end
+end
