b59c095a1640b8b48c8f8dc7efd7bae38ab034f1
Tran Son Tung <tungts1101@gmail.com>
2019-11-21 08:07:37 +0000

    hot fix

--- mizugi/app/models/user.rb
+++ mizugi/app/models/user.rb
@@ -3,12 +3,6 @@ class User < ApplicationRecord
          :rememberable, :validatable, :confirmable, :recoverable
   
   has_many :credit_cards, dependent: :destroy
-  after_commit :assign_customer_id, on: :create
 
-  def assign_customer_id
-    customer = Stripe::Customer.create email: email
-    self.customer_id = customer.id
-  end
-  
   selecting :product_feature
 end
--- mizugi/db/schema.rb
+++ mizugi/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_040845) do
+ActiveRecord::Schema.define(version: 2019_11_21_061348) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2019_11_21_040845) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_feature_id"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "product_feature_id"], name: "index_carts_on_user_id_and_product_feature_id"
   end
 
   create_table "categories", force: :cascade do |t|
