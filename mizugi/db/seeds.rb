fce884418c829b734837a1f0044d3f5fb2ea87c5
Tran Son Tung <tungts1101@gmail.com>
2019-11-25 13:11:26 +0000

    seed db

--- mizugi/db/seeds.rb
+++ mizugi/db/seeds.rb
@@ -35,14 +35,40 @@ categories.each do |name|
 end
   
 products = [
+   ["Couple clothes 1", "This is one clothes", 1],
+   ["Couple clothes 2", "This is one clothes", 1],
+   ["Couple clothes 3", "This is one clothes", 1],
+   ["Couple clothes 4", "This is one clothes", 1],
+   ["Couple clothes 5", "This is one clothes", 1],
+   ["Couple clothes 6", "This is one clothes", 1],
+   ["Couple clothes 7", "This is one clothes", 1],
+   ["Couple clothes 8", "This is one clothes", 1],
    ["Easy Polo Christmas Edition", "Easy Polo Christmas Edition Description", 2],
    ["addidas New Hammer", "addidas New Hammer solefor Sports person", 2],
    ["Easy Polo Black Edition", "Easy Polo Black Edition Description", 2],
    ["Easy Polo Christmas Edition", "Easy Polo Christmas Edition Description", 2],
+   ["Equipment 1", "This is an equipment", 3],
+   ["Equipment 2", "This is an equipment", 3],
+   ["Equipment 3", "This is an equipment", 3],
+   ["Equipment 4", "This is an equipment", 3],
+   ["Equipment 5", "This is an equipment", 3],
+   ["Equipment 6", "This is an equipment", 3]
 ]
 
+def img_url category_id
+    case category_id
+    when 1
+        Rails.root.join("public/images/products/couple#{rand(1..8)}.jpg")
+    when 2
+        Rails.root.join("public/images/products/p#{rand(1..8)}.jpg")
+    when 3
+        Rails.root.join("public/images/products/gym#{rand(1..10)}.jpg")
+    end
+end
+
 products.each do |name, description, category_id|
-    Product.create(name: name, description: description, category_id: category_id)
+    Product.create(name: name, description: description, category_id: category_id,
+                   images: Array.new(1) { img_url(category_id).open })
 end
 
 size = ['S','M','L','XL','XXL']
