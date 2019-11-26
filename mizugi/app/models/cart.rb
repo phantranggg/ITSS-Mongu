9350207cdea448418416d3c90e26853acc029d3f
Tran Son Tung <tungts1101@gmail.com>
2019-11-26 08:38:59 +0000

    Fix style cart page

--- mizugi/app/models/cart.rb
+++ mizugi/app/models/cart.rb
@@ -1,7 +1,9 @@
 class Cart < ApplicationRecord
-belongs_to :user
-belongs_to :product_feature
-
-validates :user_id, presence: true
-validates :product_feature_id, presence: true
-end
+    belongs_to :user
+    belongs_to :product_feature
+    
+    validates :user_id, presence: true
+    validates :product_feature_id, presence: true
+    
+    
+end 
