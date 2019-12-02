2f71c1a54424dab814f4b91a35500d2c5734f3d2
TheVu <thevudoan@gmail.com>
2019-12-02 10:13:27 +0700

    Update user.rb

--- mizugi/app/models/user.rb
+++ mizugi/app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable, :confirmable, :recoverable
-<<<<<<< HEAD
+
   has_many :comments
-=======
-  
   has_many :ratings
   has_many :credit_cards, dependent: :destroy
 
@@ -14,5 +12,4 @@ class User < ApplicationRecord
     self.carts.
       inject(0) {|result, cart| result + cart.product_feature.price * cart.quantity}
   end
->>>>>>> 964cd461c7eb63ea3e4f56fab29179148966c27a
 end
