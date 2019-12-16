ed4748869c8e8fbee3ee8305e6182c21909943c1
Doan The Vu <thevudoan@gmail.com>
2019-12-16 03:55:45 +0000

    fix display

--- mizugi/db/schema.rb
+++ mizugi/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_11_015139) do
+ActiveRecord::Schema.define(version: 2019_12_13_032931) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -76,6 +76,14 @@ ActiveRecord::Schema.define(version: 2019_12_11_015139) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "product_custom_logos", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.string "logo_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "product_features", force: :cascade do |t|
